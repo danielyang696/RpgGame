a849a923ea95cc95e497979b8dd5593d05e5f863
danielyang696 <879455487a@gmail.com>
2025-12-20 16:45:17 +0800

    try to chage to c++ project

---
+++ Plugins/MovementUtils/Source/MovementUtils/Private/MovementUtilsBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MovementUtilsBPLibrary.h"
+#include "MovementUtils.h"
+
+UMovementUtilsBPLibrary::UMovementUtilsBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UMovementUtilsBPLibrary::MovementUtilsSampleFunction(float Param)
+{
+	return -1;
+}
+
